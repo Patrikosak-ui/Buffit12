b4ad2c15fbaec8a6f8e3a52875ec0e85f45568c0
Patrikosak-ui <129952490+Patrikosak-ui@users.noreply.github.com>
2023-11-27 20:50:14 +0100

    registter/login

---
+++ login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Přihlášení</title>
+</head>
+<body>
+
+    <main>
+        <h2>Přihlášení</h2>
+        <form action="login.php" method="post">
+            Email: <input type="email" name="email" required><br>
+            Heslo: <input type="password" name="heslo" required><br>
+            <input type="submit" value="Přihlásit">
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Připojení k databázi
+            $host = "localhost";
+            $db_name = "buffit";
+            $username = "root";
+            $password = "";
+
+            try {
+                $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo "Chyba připojení k databázi: " . $e->getMessage();
+            }
+
+            $email = $_POST["email"];
+            $heslo = $_POST["heslo"];
+
+            // Příprava a provedení SQL dotazu pro ověření přihlašovacích údajů
+            $sql = "SELECT * FROM users WHERE email = :email";
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindParam(':email', $email);
+
+            try {
+                $stmt->execute();
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($user && password_verify($heslo, $user['heslo'])) {
+                    echo "Přihlášení úspěšné. Vítejte, " . $user['jmeno'] . "!";
+                    // Zde byste měli nastavit relaci a přesměrovat uživatele na jejich domovskou stránku.
+                } else {
+                    echo "Neplatné přihlašovací údaje. Zkuste to znovu nebo <a href='register.html'>se zaregistrujte</a>.";
+                }
+            } catch (PDOException $e) {
+                echo "Chyba při přihlašování: " . $e->getMessage();
+            }
+        }
+        ?>
+
+    </main>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="register.css">
+    <title>Registrace</title>
+</head>
+<body>
+
+<main>
+    <h2>Registrace</h2>
+
+    <form action="register.php" method="post">
+        Jméno: <input type="text" name="jmeno" required><br>
+        Příjmení: <input type="text" name="prijmeni" required><br>
+        Email: <input type="email" name="email" required><br>
+        Heslo: <input type="password" name="heslo" required><br>
+        <input type="submit" value="Registrovat">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Připojení k databázi
+        $host = "localhost";
+        $db_name = "buffit";
+        $username = "root";
+        $password = "";
+
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Chyba připojení k databázi: " . $e->getMessage();
+        }
+
+        // Získání hodnot z formuláře
+        $jmeno = $_POST["jmeno"];
+        $prijmeni = $_POST["prijmeni"];
+        $email = $_POST["email"];
+        $heslo = password_hash($_POST["heslo"], PASSWORD_DEFAULT); // Hashování hesla
+
+        // Příprava a provedení SQL dotazu pro vložení do tabulky users
+        $sql = "INSERT INTO users (jmeno, prijmeni, email, heslo) VALUES (:jmeno, :prijmeni, :email, :heslo)";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':jmeno', $jmeno);
+        $stmt->bindParam(':prijmeni', $prijmeni);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':heslo', $heslo);
+
+        try {
+            $stmt->execute();
+            echo "Registrace úspěšná. Nyní se můžete <a href='login.php'>přihlásit</a>.";
+        } catch (PDOException $e) {
+            echo "Chyba při registraci: " . $e->getMessage();
+        }
+    }
+    ?>
+</main>
+
+</body>
+</html>
